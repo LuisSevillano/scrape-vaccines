7c529fecd97d3f1fc3b5432dce82e69b6ea0e1d2
luissevillano <luissevillanopires@gmail.com>
2017-06-27 12:35:16 +0200

    adding gitignore

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-// I ended up using promises in this example because of the asynchronicity of all the requests -
-// I ran into a problem where I was calling my makeCSV function before I had pushed all of the
-// info I wanted into countryInfo.
 var Promise = require('bluebird');
 var request = require('request-promise');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var d3 = require('d3');
+var d3Dsv = require('d3-dsv');
 
 var url = 'https://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
 
-/// Gathers links corresponding to each animal's individual info page.
+/// Gathers links corresponding to each country individual info page.
 function scrapeLinks() {
-	// var startingUrl = 'http://www.midwestanimalrescue.org/animals/browse?special=Kittens';
 	var startingUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do';
 	var countryUrls = [];
 
 	// Makes a request to the startingUrl and then, from the response (which is the startingUrl's HTML),
-	// gathers all of the links corresponding to each animal's individual page.
+	// gathers all of the links corresponding to each country individual page.
 	request(startingUrl, function (error, response, body) {
 		// var baseUrl = 'http://www.midwestanimalrescue.org';
 		var baseUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
@@ -25,12 +21,9 @@ function scrapeLinks() {
 		if (error) {
 			throw new Error(error);
 		}
-		// 'browse' is the class associated with each animal's previewed info. This collects every chunk of HTML with the class
-		// 'browse' and then for each chunk finds the 'a' tag and takes the value of the 'href' attribute,
-		// which is something like "/animals/detail?AnimalID=10088333". It then adds this specific Url path to the
-		// domain Url (http://www.midwestanimalrescue.org) and saves the concatenation as a link to a specific animal.
-		// rm first el
 
+		// gets urls of each country from the Select HTML element without first one which has not data associated
+		// and fill and array
 		$('#pais > option:not(:selected)').each(function(i, element) {
 			$ = cheerio.load(element);
 
@@ -42,8 +35,8 @@ function scrapeLinks() {
 	});
 }
 
-// Makes a request to each link corresponding to an animal's page and creates an object based on that animal's
-// information, and then sends that object to makeCSV to be put into CSV/spreadsheet format.
+// Makes a request to each link corresponding to a country page and creates an object based on that country
+// information, and then sends that object to makeTSV to be put into TSV/spreadsheet format.
 function scrapeInfo(countryUrls) {
 	var countryInfo = [];
 	Promise.map(countryUrls, function(countryUrl) {
@@ -69,6 +62,7 @@ function scrapeInfo(countryUrls) {
 				"Vacunas recomendadas": "Es conveniente tener actualizado el calendario oficial de vacunaciones. Pueden ser recomendables otras vacunas, cuya prescripción deberá realizarse de forma personalizada en cualquiera de los Centros de Vacunación Internacional autorizados.",
 				"Cancillería Española": "",
 			};
+
 			var countryName = toTitleCase($("h2").text());
 			var tables = $('.imagen_texto table'),
 			details = tables[0],
@@ -76,20 +70,23 @@ function scrapeInfo(countryUrls) {
 			vacReccom = tables[2];
 			paludismo = tables[3];
 
-			// set country name
+			// get country name
 			countryObj["Nombre"] = countryName;
 			$(details).find("tr").each(function(d, i){
+
 				var property = $(this).find("th").text().trim(),
 				value = $(this).find("td").text().trim();
+
 				countryObj[property] = value;
 			});
 
-			// country has paludismo table
+			// If this country has paludismo info table
 			if(paludismo){
 				countryObj["Paludismo"] = $(paludismo).find("td").text().trim();
 			}
 			countryObj["Vacunas exigidas"] = $(vacRequired).find("td").text().trim();
 			countryObj["Vacunas recomendadas"] = $(vacReccom).find("td").text().trim();
+
 
 			countryInfo.push(countryObj);
 		});
@@ -97,34 +94,12 @@ function scrapeInfo(countryUrls) {
 	.then(function() {
 		// only calles this function once all of the countryObj have been pushed into
 		// countryInfo
-		makeCSV(countryInfo);
+		makeTSV(countryInfo);
 	});
 }
 
-function makeCSV(countryInfo) {
-	var animalObj = countryInfo[0];
-	// console.log(animalObj);
-	var firstRow = "";
-	for (var key in animalObj) {
-		firstRow += (key + "\t");
-	}
-
-	fs.writeFile('countryInfo.tsv', d3.tsvFormat(countryInfo));
-	// fs.appendFile('countryInfo.tsv', (firstRow + "\n"));
-	// var keys = firstRow.split("\t");
-	// for (var i = 0; i < countryInfo.length; i++) {
-	// 	var thisRow = "";
-	// 	for (var j = 0; j < keys.length; j++) {
-	// 		console.log(keys[j]);
-	// 		if (countryInfo[i][keys[j]]) {
-	// 			thisRow += (countryInfo[i][keys[j]] + "\t");
-	// 		}
-	// 		else {
-	// 			thisRow += "\t";
-	// 		}
-	// 	}
-	// 	fs.appendFile('countryInfo.tsv', (thisRow + "\n"));
-	// }
+function makeTSV(countryInfo) {
+	fs.writeFile('countryInfo.tsv', d3Dsv.tsvFormat(countryInfo));
 }
 function toTitleCase(str) {
 	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
