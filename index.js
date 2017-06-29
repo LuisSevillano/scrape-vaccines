537d8537a6426176025e73b2c14c2ddd2ffe1908
Luis <luissevillanopires@gmail.com>
2017-06-29 11:18:48 +0200

    matching with ISO codes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var Promise = require('bluebird');
 var request = require('request-promise');
 var cheerio = require('cheerio');
@@ -6,133 +7,134 @@ var d3Dsv = require('d3-dsv');
 var mkdirp = require('mkdirp');
 var d3 = require("d3");
 
-var url = 'https://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
+var filePath = 'ISO_3166-1.tsv';
+fs.readFile(filePath, 'utf8', function(error, data) {
 
-/// Gathers links corresponding to each country individual info page.
-function scrapeLinks() {
-	var startingUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do';
-	var countryUrls = [];
+	var url = 'https://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
 
-	// Makes a request to the startingUrl and then, from the response (which is the startingUrl's HTML),
-	// gathers all of the links corresponding to each country individual page.
-	request(startingUrl, function (error, response, body) {
-		var baseUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
-		var $ = cheerio.load(body);
-		// basic error handling
-		if (error) {
-			throw new Error(error);
-		}
+	/// Gathers links corresponding to each country individual info page.
+	function scrapeLinks() {
+		var startingUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do';
+		var countryUrls = [];
 
-		// get urls of each country from the Select 'HTML element' without the default option which has no data associated
-		// and fill and array
-		$('#pais > option:not(:selected)').each(function(i, el) {
-			$ = cheerio.load(el);
-
-			var countryLinkId = $(this).val();
-			countryUrls[i] = baseUrl + countryLinkId;
-		});
-
-		scrapeInfo([countryUrls[0]]);
-	});
-}
-
-// Makes a request to each link corresponding to a country page and creates an object based on that country
-// information, and then sends that object to makeTSV to be put into TSV/spreadsheet format.
-function scrapeInfo(countryUrls) {
-	var countryInfo = [];
-	Promise.map(countryUrls, function(countryUrl) {
-
-		return request(countryUrl, function(error, response, body) {
-			// first of all I was testing this individual code for each page using chrome's Snippets from the dev tools
-			// to prevent be banned/blocked because of make multiple requests
+		// Makes a request to the startingUrl and then, from the response (which is the startingUrl's HTML),
+		// gathers all of the links corresponding to each country individual page.
+		request(startingUrl, function(error, response, body) {
+			var baseUrl = 'http://www.msssi.gob.es/profesionales/saludPaises.do?metodo=verDetallePais&pais=';
 			var $ = cheerio.load(body);
-
+			// basic error handling
 			if (error) {
 				throw new Error(error);
 			}
-			// this object has some default options filled if those fields doesn't exist on individual page
-			var countryObj = {
-				"codISO":"",
-				"Nombre": "",
-				"Paludismo": "No existe riesgo de paludismo",
-				"Vacunas exigidas": "Ninguna",
-				"Vacunas recomendadas": "Ninguna",
-				"Capital": "",
-				"Lengua": "",
-				"Altitud": "",
-				"Población": "",
-				"Moneda": "",
-				"Zona horaria": "",
-				"Clima": "",
-				"Cancillería Española": "",
-				"URL": countryUrl,
-				"pageId":countryUrl.substring(countryUrl.lastIndexOf('=') + 1)
-			};
-			var countryName = toTitleCase($("h2").text());
-			var tables = $('.imagen_texto table'),
-			details = tables[0],
-			vacRequired = tables[1],
-			vacReccom = tables[2];
-			paludismo = tables[3];
 
-			tables.each(function(d){
-				var tableHeader = $(this).find("th").text().trim();
-				if(tableHeader == "VACUNAS EXIGIDAS"){
-					vacRequired = this;
-					countryObj["Vacunas exigidas"] = $(this).find("td").text().trim();
-				}
-				else if(tableHeader == "VACUNAS RECOMENDADAS"){
-					vacReccom = this;
-					countryObj["Vacunas recomendadas"] = $(vacReccom).find("td").text().trim();
-				}
-				else if(tableHeader == "Paludismo"){
-					paludismo = this;
-					countryObj["Paludismo"] = $(paludismo).find("td").text().trim()
-				}
-			})
+			// get urls of each country from the Select 'HTML element' without the default option which has no data associated
+			// and fill and array
+			$('#pais > option:not(:selected)').each(function(i, el) {
+				$ = cheerio.load(el);
 
-			fs.readFile('ISO_3166-1.tsv', 'utf8', function(error, tsv) {
-			  data = d3.tsvParse(tsv);
-				data.forEach(function(d){
+				var countryLinkId = $(this).val();
+				countryUrls[i] = baseUrl + countryLinkId;
+			});
+
+			scrapeInfo(countryUrls);
+		});
+	}
+
+	// Makes a request to each link corresponding to a country page and creates an object based on that country
+	// information, and then sends that object to makeTSV to be put into TSV/spreadsheet format.
+	function scrapeInfo(countryUrls) {
+		var countryInfo = [];
+		Promise.map(countryUrls, function(countryUrl) {
+
+			return request(countryUrl, function(error, response, body) {
+				// first of all I was testing this individual code for each page using chrome's Snippets from the dev tools
+				// to prevent be banned/blocked because of make multiple requests
+				var $ = cheerio.load(body);
+
+				if (error) {
+					throw new Error(error);
+				}
+				// this object has some default options filled if those fields doesn't exist on individual page
+				var countryObj = {
+					"codISO": "",
+					"Nombre": "",
+					"Paludismo": "No existe riesgo de paludismo",
+					"Vacunas exigidas": "Ninguna",
+					"Vacunas recomendadas": "Ninguna",
+					"Capital": "",
+					"Lengua": "",
+					"Altitud": "",
+					"Población": "",
+					"Moneda": "",
+					"Zona horaria": "",
+					"Clima": "",
+					"Cancillería Española": "",
+					"URL": countryUrl,
+					"pageId": countryUrl.substring(countryUrl.lastIndexOf('=') + 1)
+				};
+				var countryName = toTitleCase($("h2").text());
+				var tables = $('.imagen_texto table'),
+				details = tables[0],
+				vacRequired = tables[1],
+				vacReccom = tables[2];
+				paludismo = tables[3];
+
+				tables.each(function(d) {
+					var tableHeader = $(this).find("th").text().trim();
+					if (tableHeader == "VACUNAS EXIGIDAS") {
+						vacRequired = this;
+						countryObj["Vacunas exigidas"] = $(this).find("td").text().trim();
+					} else if (tableHeader == "VACUNAS RECOMENDADAS") {
+						vacReccom = this;
+						countryObj["Vacunas recomendadas"] = $(vacReccom).find("td").text().trim();
+					} else if (tableHeader == "Paludismo") {
+						paludismo = this;
+						countryObj["Paludismo"] = $(paludismo).find("td").text().trim()
+					}
+				})
+				var tsv = d3.tsvParse(data);
+
+				tsv.forEach(function(d) {
 					var codISO = d.code,
 					pageId = d.pageId;
 					if (countryObj["pageId"] == pageId) {
-						console.log("---MATCH!!!");
 						countryObj["codISO"] = codISO;
-						console.log(countryObj["codISO"]);
 						return;
 					}
 				})
-			})
 
 
 
-			// set country name
-			countryObj["Nombre"] = countryName;
-			$(details).find("tr").each(function(d, i){
-				var property = $(this).find("th").text().trim(),
-				value = $(this).find("td").text().trim();
-				countryObj[property] = value;
+				// set country name
+				countryObj["Nombre"] = countryName;
+				$(details).find("tr").each(function(d, i) {
+					var property = $(this).find("th").text().trim(),
+					value = $(this).find("td").text().trim();
+					countryObj[property] = value;
+				});
+
+				// add a new country to the main array
+				countryInfo.push(countryObj);
 			});
-
-			// add a new country to the main array
-			countryInfo.push(countryObj);
+		})
+		.then(function() {
+			// only calles this function once all of the countryObj have been pushed into countryInfo
+			makeTSV(countryInfo);
 		});
-	})
-	.then(function() {
-		// only calles this function once all of the countryObj have been pushed into countryInfo
-		makeTSV(countryInfo);
-	});
-}
+	}
 
-function makeTSV(countryInfo) {
-	mkdirp('data', function (err) {
-		if (err) console.error(err)
-		else fs.writeFile('data/countryInfo.tsv', d3Dsv.tsvFormat(countryInfo));
-	});
+	function makeTSV(countryInfo) {
+		mkdirp('data', function(err) {
+			if (err) console.error(err)
+			else fs.writeFile('data/countryInfo.tsv', d3Dsv.tsvFormat(countryInfo));
+		});
 
-}
-function toTitleCase(str) {
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-scrapeLinks();
+	}
+
+	function toTitleCase(str) {
+		return str.replace(/\w\S*/g, function(txt) {
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		});
+	}
+	scrapeLinks();
+});
